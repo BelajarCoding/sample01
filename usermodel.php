81d402a8e77526e864dd97dec80e54cfdc479e9c
vdhkb <vdhkb@users.noreply.github.com>
2014-06-19 10:54:12 +0700

    Create usermodel.php
    lalu simpan di application/models/usermodel.php

---
+++ usermodel.php
@@ -0,0 +1,27 @@
+<?php
+
+class Usermodel extends CI_Model
+{
+    function insert_data_user($data)
+    {
+        $this->db->insert('user',$data);
+    }
+
+    function get_user_by_id($id)
+    {
+        $this->db->where('user_id',$id);
+        return $this->db->get('user');
+    }
+
+    function update_data_user($data,$id)
+    {
+        $this->db->where('user_id',$id);
+        $this->db->update('user',$data);
+    }
+
+    function delete_user($id)
+    {
+        $this->db->where('user_id',$id);
+        $this->db->delete('user');
+    }
+}
